d2be0576b71882ed7410adcd5eb20e8988880fb4
Joseph Malham <110071057+jdmalham@users.noreply.github.com>
2023-01-25 15:25:49 -0800

    Add files via upload

---
+++ solids.py
@@ -0,0 +1,19 @@
+import time
+
+def solid_dual_render(path):
+    path_list = path.split('\\')
+    file_name = path_list[-1].split('.')[0]
+    with open(path, 'r+') as file, open(f'test_{file_name}.obj','w') as fout:
+        contents = file.readlines()
+        new_contents = []
+        for index, item in enumerate(contents):
+            new_contents.append(item)
+            if item[0:2] == "f ":
+                line_items = item.split(' ')
+                print(line_items)
+                line_escape_removed = line_items[3].replace('\n','')
+                added_line = f'f {line_escape_removed} {line_items[2]} {line_items[1]}\n'
+                new_contents.append(added_line)
+        fout.writelines(new_contents)
+
+solid_dual_render(r"C:\Users\Owner\Downloads\MuonChambers_V1 (2).obj")
